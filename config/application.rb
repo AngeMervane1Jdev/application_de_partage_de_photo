e3ddc7830b93de0991fbf82f8ec914a049ff9073
Ange Noutchogbe <onpasse1@gmail.com>
2021-07-16 22:35:59 +0000

    add default local as japon

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module Blogapp
     config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+    config.i18n.default_locale = :ja
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
     config.generators do |g|
