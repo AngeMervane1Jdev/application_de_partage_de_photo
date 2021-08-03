7dd48ea9a2101d97120db5367da37f20d5984b24
Noutchogbe Cyrille <angenoutchogbe@gmail.com>
2021-08-03 16:25:36 +0000

    init aws

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -2,9 +2,9 @@ $worker  = 2
 $timeout = 30
 #自分のアプリケーション名（currentがつくことに注意）
 $app_dir = "/var/www/achieve/current"
-$listen  = File.expand_path 'tmp/sockets/unicorn.sock', $app_dir
-$pid     = File.expand_path 'tmp/pids/unicorn.pid', $app_dir
-$std_log = File.expand_path 'log/unicorn.log', $app_dir
+$listen  = File.expand_path '/shared/tmp/sockets/unicorn.sock', $app_dir
+$pid     = File.expand_path '/shared/tmp/pids/unicorn.pid', $app_dir
+$std_log = File.expand_path '/shared/log/unicorn.log', $app_dir
 # 上記で設定したものが適応されるよう定義
 worker_processes  $worker
 working_directory $app_dir
